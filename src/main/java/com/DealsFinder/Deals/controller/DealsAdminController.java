cbfde4ae24077c5f3b93bd15220905477884fa3b
Premal-Miglani <83827280+Premal-Miglani@users.noreply.github.com>
2022-03-11 21:41:10 +0530

    Commited with Juit Testing

--- src/main/java/com/DealsFinder/Deals/controller/DealsAdminController.java
+++ src/main/java/com/DealsFinder/Deals/controller/DealsAdminController.java
@@ -4,20 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.ModelAndView;
 
 import com.DealsFinder.Deals.model.Deals;
 import com.DealsFinder.Deals.service.DealsAdminService;
 
 @RestController
+
 public class DealsAdminController {
 	
 	@Autowired
--- src/test/java/com/DealsFinder/Deals/DealsApplicationTests.java
+++ src/test/java/com/DealsFinder/Deals/DealsApplicationTests.java
@@ -1,13 +1,53 @@
 package com.DealsFinder.Deals;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.DealsFinder.Deals.model.Deals;
+import com.DealsFinder.Deals.repository.DealRepo;
 
 @SpringBootTest
 class DealsApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	DealRepo dealRepo;
+	
+	public void testCreate() {
+		Deals d = new Deals();
+		d.setId("151");
+		d.setItem("Books");
+		d.setDeal("Get 50% off on any comic books");
+		dealRepo.save(d);
+		assertNotNull(dealRepo.findById("151").get());
 	}
+	
+	@Test
+	public void testGetAllDeals() {
+		List<Deals> l = dealRepo.findAll();
+		assertThat(l).size().isGreaterThan(0);
+	}
+	
+//	@Test
+//	public void testSingleDeals() {
+//		Deals deals=dealRepo.findById("151").get();
+//		assertEquals("Get 50% off on any comic books",deals.getDeal());
+//	}
+//	
 
+//	@Test
+//	public void testUpdate() {
+//	
+//		Deals d = dealRepo.findById("151").get();
+//		d.setItem("Dress");
+//		dealRepo.save(d);
+//		assertNotEquals("Books",dealRepo.findById("151").get().getItem());
+//	}
 }
